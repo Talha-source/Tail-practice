f01ad39bfff38d98b401a33fe3dc8f20801bc01b
landing page <talhadeveloper.1@gmail.com>
2024-07-29 21:34:19 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var arr = [1,2,3,4,5,6,7,8,9,10];
+arr.pop()
+console.log(arr)
+
+
+var rr = ["apple","banana","mango"];
+rr.shift()
+rr.unshift("strawbery");
+console.log(rr) 
+
+ var a = 5;
+ console.log(a+5);
+ var b = 10;
+ console.log(b-5);
